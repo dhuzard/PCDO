3268da468631edb2e291f50f0132ee42c707c248
DHuzard <damien.huzard@gmail.com>
2025-09-23 16:11:04 +0200

    Initial commit

---
+++ _dump_xlsx.py
@@ -0,0 +1,10 @@
+﻿import sys
+from openpyxl import load_workbook
+wb = load_workbook("fields table.xlsx", data_only=True)
+ws = wb.active
+rows = list(ws.iter_rows(values_only=True))
+for r in rows:
+    if r is None:
+        continue
+    vals = ["" if v is None else str(v).replace('\t',' ').replace('\n',' ') for v in r]
+    print('\t'.join(vals))
